6305190de43a61b92cdb1e6777c26f657d5fe943
IliyaKr <99329456+IliyaKr@users.noreply.github.com>
2022-04-06 11:22:26 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print ("what is your name?")
+nameUser = input()
+print("How old are you?")
+ageUser = input()
+print("Where are you live?")
+cityUser = input()
+print("This is {0}".format(nameUser))
+print("It is {0}".format(ageUser))
+print("s(he) lives in {0}".format(cityUser))
